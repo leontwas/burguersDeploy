d53a0376bdce61cdce9030afd176642a2a792aa8
leontwas <140932426+leontwas@users.noreply.github.com>
2024-07-03 19:02:17 -0300

    primer commit

---
+++ server/Server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import ReservasRoutes from '../routes/reservas.routes.js'
+
+export default class Server {
+
+    static app = express()
+
+    static routes(){
+        const reservas = new ReservasRoutes()
+        Server.app.use('/reservas', reservas.router )
+        Server.app.get('/', (req, res) => {
+            res.send('Hola')
+})
+    }
+
+    static runServer(port){
+        Server.app.listen(port, () =>
+            console.log(`Escuchando en http://localhost:${port}`))
+    }  
+
+    static run(port) {  
+        console.clear()
+        Server.routes()
+        Server.runServer(port)
+        }
+    }
+     
