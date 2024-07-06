fc0c1c22ad515d10c982799bfc2b0bbe60476c5c
leontwas <140932426+leontwas@users.noreply.github.com>
2024-07-06 01:40:36 -0300

    agregué el archivo crearDB.js para crear mi base de datos

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,13 +1,9 @@
 export default class Reserva {
-  constructor(cliente_id, nombre, apellido, telefono, email, fecha, comensales, menores) {
-    this.cliente_id = cliente_id;
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.telefono = telefono;
-    this.email = email;
+  constructor(reserva_id,	cliente_id,	mesa_id, fecha_reserva,	estado) {
+    this.reserva_id = null;
+    this.cliente_id = null;
+    this. mesa_id= null;
     this.fecha = fecha;
-    this.comensales = comensales;
-    this.menores = menores;
-    this.estado = 'Pendiente'; // Estado por defecto al crear la reserva
+    this.estado = true // Estado por defecto al crear la reserva
 }
 }
