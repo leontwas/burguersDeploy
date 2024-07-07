e21d5a6b6ece3509891f8c1cdac23d8ffe53f619
leontwas <140932426+leontwas@users.noreply.github.com>
2024-07-07 20:25:32 -0300

    mejorando los constructores

--- models/Mesa.js
+++ models/Mesa.js
@@ -1,8 +1,8 @@
 export default class Reserva {
-    constructor(mesa_id, numero, capacidad, disponible) {
+    constructor(mesa_id = null, numero, capacidad, disponible = true) {
       this.mesa_id = mesa_id;
       this.numero = numero;
       this.capacidad = capacidad;
-      this.disponible = true;
+      this.disponible = disponible;
     }
 }
