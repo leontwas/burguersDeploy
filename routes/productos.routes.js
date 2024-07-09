e521ad2ed3e90f21b175b4ca52689dfae6747c18
leontwas <140932426+leontwas@users.noreply.github.com>
2024-07-09 02:12:15 -0300

    Update productos.routes.js

--- routes/productos.routes.js
+++ routes/productos.routes.js
@@ -1,5 +1,5 @@
 import ProductosMysqlControllers from '../controllers/productos.mysql.controllers.js';
-import Routes from './Routes';
+import Routes from './Routes.js';
 import { Router } from 'express';
 
 export default class ProductosRoutes extends Routes {
