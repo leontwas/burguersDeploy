a6fadd241f1532b6f2d5f247230882a2c5c11958
leontwas <140932426+leontwas@users.noreply.github.com>
2024-07-09 02:41:25 -0300

    Update mesas.routes.js

--- routes/mesas.routes.js
+++ routes/mesas.routes.js
@@ -1,12 +1,12 @@
-import MesasControllers from '../controllers/mesas.controllers.js';
-import Routes from './routes.js';
+import MesasMysqlControllers from '../controllers/mesas.mysql.controllers.js';
+import Routes from './Routes.js';
 import { Router } from 'express';
 
 export default class MesasRoutes extends Routes {
     constructor() {
         super();
         this.router = Router();
-        this.controller = new MesasControllers();
+        this.controller = new MesasMysqlControllers();
         this.getRoutes();
     }
 
@@ -14,7 +14,7 @@ export default class MesasRoutes extends Routes {
         this.router
         .get('/', this.controller.getAllMesas)
         .get('/id', this.controller.getMesaById)
-        .post('/', this.controller.createMesa)
+        .post('/', this.controller.addMesa)
         .put('/id', this.controller.updateMesa)
         .delete('/id', this.controller.deleteMesa)
     }
