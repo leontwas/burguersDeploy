930d356eaed23afcd6bbb06ad3b7dbb0e5098fc4
leontwas <140932426+leontwas@users.noreply.github.com>
2024-07-10 14:14:42 -0300

    Update mysql.config.js

--- db/config/mysql.config.js
+++ db/config/mysql.config.js
@@ -1,28 +1,11 @@
-// const config = {
-//    host: 'sql3.freesqldatabase.com',
-//    port: 3306,
-//    user: 'sql3718809',
-//    password: '1PtpLX6Yiz',
-//    database: 'sql3718809'
-// };
-
 const config = {
-    host: process.env.HOST_DB || 'localhost',
-    port: process.env.PORT || 3000,
-    user: process.env.USER_DB || 'root',
-    password: process.env.PASS_DB || 'root',
-    database:process.env.DB || 'hamburgueseria'
+    host: process.env.HOST_DB,
+    user: process.env.USER_DB,
+    password: process.env.PASS_DB,
+    database: process.env.DB_NAME,
+    port: process.env.PORT_DB,
+    connectTimeout: 10000  // Aumenta el tiempo de espera a 10 segundos
 }
 
-//    const config = {
-//    host: 'localhost',
-//    port: 3000,
-//    user: 'root',
-//    password: 'root',
-//    database: 'hamburgueseria'
-// }
-
-
-
-
 export default config;
+
