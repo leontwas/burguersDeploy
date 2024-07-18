b2701ed14aa274e41a77f286680ce411f1eea574
leontwas <140932426+leontwas@users.noreply.github.com>
2024-07-18 17:52:52 -0300

    cambio

--- db/config/mysql.config.js
+++ db/config/mysql.config.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-    host: HOST_DB,
-    user: USER_DB,
-    password: PASS_DB,
-    database: DB,
-    PORT: PORT_DB,
+    host: process.env.HOST_DB,
+    user: process.env.USER_DB,
+    password: process.env.PASS_DB,
+    database: process.env.DB,
+    port: process.env.PORT_DB,
     connectTimeout: 10000  // Aumenta el tiempo de espera a 10 segundos
 };
 
